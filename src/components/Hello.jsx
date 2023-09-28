9a867377a8cc6f4ffd5b58fef6c1ae24f36309e8
06Kaito26 <kotenbu.hyouka@gmail.com>
2023-09-28 09:35:06 +0900

    'section9END'

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,12 +1,12 @@
 import { Text, View, StyleSheet } from "react-native";
-import { string, bool } from 'prop-types'
+import { string, bool,shape } from "prop-types";
 
 function Hello(props) {
-    const { children, bang } = props;
+    const { children, bang, style } = props;
     return (
         <View>
-            <Text style={styles.text}>
-                {`Hello ${children}${bang ? '!' : ''}`}
+            <Text style={[styles.text, style]}>
+                {`Hello ${children}${bang ? "!" : ""}`}
             </Text>
         </View>
     );
@@ -15,10 +15,12 @@ function Hello(props) {
 Hello.propTypes = {
     children: string.isRequired,
     bang: bool,
+    style: shape(),
 };
 
 Hello.defaultProps = {
-    bang:false,
+    bang: false,
+    style: null,
 };
 
 const styles = StyleSheet.create({
