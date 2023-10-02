470562e1c7d09e5a8a996142f41fd93e0f067aca
06Kaito26 <kotenbu.hyouka@gmail.com>
2023-10-02 10:19:44 +0900

    'create

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
-import MemoListScreen from "./src/screens/MemoListScreen";
+// import MemoListScreen from "./src/screens/MemoListScreen";
 // import MemoDetaileScreen from "./src/screens/MemoDetaileScreen";
+// import MemoEditScreen from "./src/screens/MemoEditScreen";
+import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 
 export default function App() {
     return (
-        <MemoListScreen />
+        // <MemoListScreen />
         // <MemoDetaileScreen />
+        // <MemoEditScreen />
+        <MemoCreateScreen />
     );
 }
