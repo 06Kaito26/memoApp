1836911e77f1fe8f10fadcfb98bd45a8016f969a
06Kaito26 <kotenbu.hyouka@gmail.com>
2023-10-03 09:11:48 +0900

    'LogIn,SignUp画面作成'

--- App.jsx
+++ App.jsx
@@ -1,13 +1,17 @@
 // import MemoListScreen from "./src/screens/MemoListScreen";
 // import MemoDetaileScreen from "./src/screens/MemoDetaileScreen";
 // import MemoEditScreen from "./src/screens/MemoEditScreen";
-import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import LogInScreen from "./src/screens/LogInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 export default function App() {
     return (
         // <MemoListScreen />
         // <MemoDetaileScreen />
         // <MemoEditScreen />
-        <MemoCreateScreen />
+        // <MemoCreateScreen />
+        // <LogInScreen />
+        <SignUpScreen />
     );
 }
