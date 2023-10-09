205a18ea151a72f0f8bc69059a6d6ee71b1ff96f
06Kaito26 <kotenbu.hyouka@gmail.com>
2023-10-09 13:57:37 +0900

    'ユーザー毎にメモを保存の実装'

---
+++ env.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+export const firebaseConfig = {
+    apiKey: "AIzaSyBhlyEUPAUSYA6Mw_0l3gAO3XcPspoke34",
+    authDomain: "memoapp-4c393.firebaseapp.com",
+    projectId: "memoapp-4c393",
+    storageBucket: "memoapp-4c393.appspot.com",
+    messagingSenderId: "588903822391",
+    appId: "1:588903822391:web:d38a4f8b897b6ee959bbb2",
+    measurementId: "G-MWXLM93V59",
+};
--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,21 +1,52 @@
 import {
-    StyleSheet, TextInput, View, KeyboardAvoidingView,
+    StyleSheet,
+    TextInput,
+    View,
+    KeyboardAvoidingView,
+    Platform,
 } from "react-native";
+import { collection, addDoc } from "firebase/firestore";
+import { useState } from "react";
+
 import CircleBotton from "../components/CircleBotton";
+import { auth, db } from "../../firebase"; // authは使わないので削除
 
 export default function MemoCreateScreen(props) {
     const { navigation } = props;
+    const user = auth.currentUser; // ログイン中のユーザー情報を取得
+    const [bodyText, setBodyText] = useState("");
+
+    const handlePress = () => {
+        addDoc(collection(db, `users/${user.uid}/memos`), {
+            bodyText,
+            updatedAt: new Date(),
+        })
+            .then((docRef) => {
+                console.log("Created!", docRef.id);
+                navigation.goBack();
+            })
+            .catch((error) => {
+                console.log("Error!", error);
+            });
+    };
+
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="height">
-            <View style={styles.inputContainer}>
-                <TextInput value="" multiline style={styles.input} />
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === "ios" ? "height" : null}
+        >
+            <View style={styles.contentContainer}>
+                <TextInput
+                    value={bodyText}
+                    multiline
+                    style={styles.input}
+                    onChangeText={(text) => {
+                        setBodyText(text);
+                    }}
+                    autoFocus
+                />
             </View>
-            <CircleBotton
-                name="check"
-                onPress={() => {
-                    navigation.goBack();
-                }}
-            />
+            <CircleBotton name="check" onPress={handlePress} />
         </KeyboardAvoidingView>
     );
 }
@@ -24,10 +55,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    inputContainer: {
+    contentContainer: {
+        flex: 1, // コンテンツの高さが画面に合わせて調整される
         paddingHorizontal: 27,
         paddingVertical: 32,
-        flex: 1,
     },
     input: {
         flex: 1,
