c5549d279a07e86680a2498d2d63320f404fb25e
06Kaito26 <kotenbu.hyouka@gmail.com>
2023-10-15 10:45:52 +0900

    '日付フォーマット修正'

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,19 @@
-import { add, format } from "date-fns";
+import { format } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
 
 // eslint-disable-next-line import/prefer-default-export
 export function dateToString(date) {
     if (!date) {
         return "";
     }
-    // 日本の時間にならないので取得した時間 + 9時間として日本時間に合わせる（原因不明）
-    const futureDate = add(date, { hours: 9 });
-    // console.log(format(futureDate, "yyyy年M月d日 HH時mm分"));
-    return format(futureDate, "yyyy年M月d日 HH時mm分", { timeZone: "Asia/Tokyo" });
+
+    // 日本のタイムゾーン ('Asia/Tokyo') に変換
+    const japanTimeZone = "Asia/Tokyo";
+    const zonedDate = utcToZonedTime(date, japanTimeZone);
+
+    // フォーマットしたい形式 ('yyyy/MM/dd HH:mm')
+    const formatString = "yyyy/MM/dd HH:mm";
+
+    // 日本のタイムゾーンで日付をフォーマット
+    return format(zonedDate, formatString, { timeZone: japanTimeZone });
 }
